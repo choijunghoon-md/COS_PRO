dabaf1cebf289eb6b2821867437918517e8d0738
CHOI_JUNG_HOON <118896663+choijunghoon-md@users.noreply.github.com>
2023-02-06 23:55:09 -0800

    Add files via upload

---
+++ 2023_02_07/Project3/Project3/main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int temperature[], int temperature_len, int A, int B) {
+	int answer = 0;
+	for (int i = A; i < B - 1; i++) {
+		if (temperature[A - 1] < temperature[i] && temperature[B - 1] < temperature[i]) {
+			answer++;
+		}
+	}
+	return answer;
+}
+
+
+int main(void) {
+
+	int temperature[] = { 3,2,1,5,4,3,3,2 };
+	int temperature_len = 8;
+	int A = 1;
+	int B = 6;
+	int ret = solution(temperature, temperature_len, A, B);
+
+	printf("solution 함수의 반환 값은 %d 입니다. \n", ret);
+
+	return 1;
+}
