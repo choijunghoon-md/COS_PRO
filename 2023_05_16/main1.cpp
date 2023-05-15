2bf39ed1cee27b990edfda7e837929a6fe5d6f70
CHOI_JUNG_HOON <118896663+choijunghoon-md@users.noreply.github.com>
2023-05-16 08:26:33 +0900

    Add files via upload

---
+++ 2023_05_16/main1.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct Student {
+	char name[20];
+	int score;
+}Student;
+
+int compare(const void* a, const void* b) {
+	Student frist = *(Student*)a;
+	Student second = *(Student*)a;
+	if (frist.score > second.score) return 1;
+	else if (frist.score < second.score) return -1;
+	else return 0;
+}
+
+char* solution(const char*names[], int names_len, int scores[],int scores_len, int k) {
+	int len = names_len;
+	Student* students = (Student*)malloc(sizeof(Student) * names_len);
+	for (int i = 0; i < names_len; i++) {
+		strcpy(students[i].name, names[i]);
+		students[i].score = scores[i];
+	}
+	qsort(students, len, sizeof(Student), compare);
+	return students[k-1].name;
+}
+
+int main() {
+	const char* names[5] = { "lukas","james","levi","eli","max" };
+	int names_len = 5;
+	int scores[5] = { 30,20,50,40,10 };
+	int scores_len = 5;
+	int k = 2;
+	char* ret = solution(names,names_len,scores,scores_len,k);
+
+	printf("solution 함수의 변환 값은 %s 입니다.\n", ret);
+
+	return 0;
+}
---
+++ 2023_05_16/main3.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool solution(const char* password) {
+	bool answer = 0;
+	
+	int capital_count = 0, small_count = 0, digit_count = 0;
+	for (int i = 0; i < strlen(password); i++) {
+		if (password[i] >= 'a' && password[i] <= 'z') small_count++;
+		else if (password[i] >= 'A' && password[i] <= 'Z') digit_count++;
+		else if (password[i] >= '1' && password[i] <= '9') capital_count++;
+	}
+
+	if (capital_count * small_count * digit_count != 0) answer = true;
+	else answer = false;
+	return answer;
+}
+
+int main() {
+
+	const char* password1 = "helloworld";
+	bool ret1 = solution(password1);
+	printf("solution 함수의 변환 값은 %s 입니다.\n", ret1 ? "true" : "false");
+
+	const char* password2 = "Hello123";
+	bool ret2 = solution(password2);
+	printf("solution 함수의 변환 값은 %s 입니다.\n", ret2 ? "true" : "false");
+
+	return 0;
+}
