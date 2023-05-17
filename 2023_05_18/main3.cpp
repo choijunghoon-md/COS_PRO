0e24362e8a6312a6d214d5b72cc468374fec1c67
CHOI_JUNG_HOON <118896663+choijunghoon-md@users.noreply.github.com>
2023-05-18 08:55:58 +0900

    Update main3.cpp

--- 2023_05_18/main3.cpp
+++ 2023_05_18/main3.cpp
@@ -6,7 +6,10 @@
 int solution(int classes[],int classes_len, int m) {
 	int answer = 0;
 	for (int i = 0; i < classes_len; i++) {
-		answer += (m - classes[i] % m + classes[i])/m;
+		if( classes[i] % m != 0)
+			answer += (m - classes[i] % m + classes[i])/m;
+		else
+			answer += (classes[i])/m;
 	}
 	return answer;
 }
@@ -15,7 +18,7 @@ int main() {
 	int classes[] = { 80,45,33,20 };
 	int m = 30;
 	int ret = solution(classes, 4, m);
-	printf("solution �Լ��� ��ȯ ���� %d �Դϴ�.\n", ret);
+	printf("solution ÇÔ¼öÀÇ º¯È¯ °ªÀº %d ÀÔ´Ï´Ù.\n", ret);
 
 	return 0;
 }
